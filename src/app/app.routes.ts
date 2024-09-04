a5173a75fc80813b715d151fcfd2d61f989f9b8b
KayqueDeomedesse <ksd.deomedesse@hotmail.com>
2024-09-04 08:37:57 -0300

    fix/ add some components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+
 ];
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { Register } from '../../models/SubscriptionModel/Register';
+import { RegisterService } from '../../services/register.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  userId!: string;
 
+  constructor(
+    private registerService: RegisterService,
+    private loginService: LoginService,
+    private router: Router,
+  ) { }
+
+  public newRegister: Register = new Register();
+
+
+  registerAndLogin() {
+    console.log(this.newRegister)
+    this.registerService.post(this.newRegister).subscribe(
+      (response: any) => {
+        console.log('Temos uma nova empresa registrada!', response);
+        this.loginService.post({ email: this.newRegister.email, password: this.newRegister.password }).subscribe(
+          (loginResponse: any) => {
+            console.log('Login bem-sucedido!', loginResponse);
+            this.newRegister = new Register();
+            this.router.navigate(['/login']);
+          },
+          (loginError: any) => {
+            console.log('Erro ao fazer login!', loginError);
+          }
+        );
+      }
+    )
+  }
 }
