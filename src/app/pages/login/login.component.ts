52eeced344957ce0101c95b0d43b22a71da221a4
Pedro Henrique <henriquepedrojg18@gmail.com>
2024-09-06 15:40:22 -0300

    toast success

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,29 @@
+import { ButtonModule } from 'primeng/button';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { LoginService } from '../../services/login.service';
 import { CookiepopupComponent } from '../../components/cookiepopup/cookiepopup.component';
 import { FormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { RippleModule } from 'primeng/ripple';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CookiepopupComponent, FormsModule],
+  imports: [
+    CookiepopupComponent,
+    FormsModule,
+    ToastModule,
+    ButtonModule,
+    RippleModule,
+    CommonModule
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  providers: [MessageService],
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   cookiesAceitos: boolean;
@@ -18,9 +31,14 @@ export class LoginComponent {
   constructor(
     private router: Router,
     private cookieService: CookieService,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private messageService: MessageService
   ) {
     this.cookiesAceitos = this.cookieService.get('aceitou_cookies') === 'true';
+  }
+
+  showSuccess() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Login Successfully' });
   }
 
   public loginRequest: any = {
