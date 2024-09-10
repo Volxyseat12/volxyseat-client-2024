a0bdbd1d1432df76c31866fe427a1f87e70b3661
PedroTocantins <pedrotocantins11s@gmail.com>
2024-09-09 22:26:20 -0300

    add: subscription table

--- src/app/pages/subscription/subscription.component.ts
+++ src/app/pages/subscription/subscription.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importando o CommonModule
 import { Subscription } from '../../models/SubscriptionModel/Subscription';
 import { SubscriptionService } from '../../services/subscription.service';
 import { SubscriptionEnum } from '../../models/Enums/SubscriptionEnum';
@@ -6,13 +7,40 @@ import { SubscriptionEnum } from '../../models/Enums/SubscriptionEnum';
 @Component({
   selector: 'app-subscription',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Incluindo o CommonModule aqui
   templateUrl: './subscription.component.html',
-  styleUrl: './subscription.component.css',
+  styleUrls: ['./subscription.component.css'],
 })
 export class SubscriptionComponent {
   subscriptions: Subscription[] = [];
   subscriptionEnum = SubscriptionEnum;
+
+  plans = [
+    { name: 'Basic', price: '$9/mo', buttonText: 'Try for free' },
+    { name: 'Medium', price: '$29/mo', buttonText: 'Try for free' },
+    { name: 'Advanced', price: '$59/mo', buttonText: 'Learn more' },
+    { name: 'Custom', price: 'Contact us', buttonText: 'Learn more' }
+  ];
+
+  features = [
+    {
+      category: 'Support',
+      items: [
+        { name: '24/7 Email Support', basic: true, medium: true, advanced: true, custom: true },
+        { name: 'Live Chat', basic: false, medium: true, advanced: true, custom: true },
+        { name: 'Phone Support', basic: false, medium: false, advanced: true, custom: true }
+      ] as Array<{ name: string; [key: string]: boolean | string }>
+    },
+  ];
+
+
+  activeCategory = 'Support';
+  filteredFeatures = this.features.find(feature => feature.category === this.activeCategory)?.items || [];
+
+  setActiveCategory(category: string) {
+    this.activeCategory = category;
+    this.filteredFeatures = this.features.find(feature => feature.category === category)?.items || [];
+  }
 
   subscriptionTypes = Object.keys(SubscriptionEnum).filter((key) =>
     this.isSubscriptionEnumKey(key)
