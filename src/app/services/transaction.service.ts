c5c1910598927e931a3c3a5814c999f30c46539e
PedroTocantins <pedrotocantins11s@gmail.com>
2024-11-19 12:32:25 -0300

    cancel subscription method

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -3,33 +3,34 @@ import { environment } from '../environments/environment';
 import { ITransaction } from '../models/SubscriptionModel/ITransaction';
 import { HttpClient } from '@angular/common/http';
 import { ICreateTransactionResponse } from '../models/ICreateTransactionResponse';
-import { VolxyseatEndpoints } from '../volxyseat.endpoints';
+import { Endpoints, VolxyseatEndpoints } from '../volxyseat.endpoints';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TransactionService {
   apiUrl = environment.apiUrl;
-
+  endpoints: Endpoints = VolxyseatEndpoints.endpoints;
   constructor(private http: HttpClient) {}
 
   // Tipando o retorno como Observable<ICreateTransactionResponse>
-  post(transaction: ITransaction) {
-    const endpointUrl = VolxyseatEndpoints.endpoints.createTransaction(
-      this.apiUrl
+  post(transaction: ITransaction): Observable<ICreateTransactionResponse> {
+    const endpointUrl = this.endpoints.createTransaction(this.apiUrl);
+    return this.http.post<ICreateTransactionResponse>(
+      `${endpointUrl}`,
+      transaction
     );
-    console.log(endpointUrl);
-    
-    // Adicionando a tipagem para a resposta
-    return this.http.post<ICreateTransactionResponse>(`${endpointUrl}`, transaction);
   }
 
   // Tipando a resposta do método getById (ajuste de acordo com o que a resposta retorna)
   getById(id: any) {
-    const endpointUrl = VolxyseatEndpoints.endpoints.getTransactionById(
-      this.apiUrl,
-      id
-    );
-    return this.http.get<ICreateTransactionResponse>(endpointUrl);  // Ajuste conforme o tipo de resposta
+    const endpointUrl = this.endpoints.getTransactionById(this.apiUrl, id);
+    return this.http.get(endpointUrl);
+  }
+
+  disableTransaction(id: string) {
+    const endpointUrl = this.endpoints.disableTransation(this.apiUrl, id);
+    return this.http.put(endpointUrl, {});
   }
 }
--- src/app/volxyseat.endpoints.ts
+++ src/app/volxyseat.endpoints.ts
@@ -7,10 +7,12 @@ export interface Endpoints {
 
   getTransactionById: (apiUrl: string, id: string) => string;
   createTransaction: (apiUrl: string) => string;
-
-  logout: (apiUrl: string) => string;
+  disableTransation: (apiUrl: string, id: string) => string;
 
   createMPSubscription: (mpApiUrl: string) => string;
+  calcelMPSubscription: (apiUrl: string, id: string) => string;
+
+  logout: (apiUrl: string) => string;
 }
 
 export namespace VolxyseatEndpoints {
@@ -18,22 +20,32 @@ export namespace VolxyseatEndpoints {
 
   export const endpoints: Endpoints = {
     getSubscriptions: (apiUrl: string) => `${apiUrl}${base}/Subscription`,
+
     getSubscriptionDetails: (apiUrl: string, id: string) =>
       `${apiUrl}${base}/subscription/${id}`,
+
     openSubscription: (apiUrl: string, id: string) =>
       `${apiUrl}${base}/subscription/${id}/states/action=open`,
+
     closeSubscription: (apiUrl: string, id: string) =>
       `${apiUrl}${base}/subscription/${id}/states/action=close`,
+
     getSubscriptionById: (apiUrl: string, id: string) =>
       `${apiUrl}${base}/subscription/${id}`,
 
     getTransactionById: (apiUrl: string, id: string) =>
       `${apiUrl}${base}/Transaction/${id}`,
-    createTransaction: (apiUrl: string) => `${apiUrl}${base}/Transaction/`,
 
-    logout: (apiUrl: string): string => `${apiUrl}${base}/User/logout`,
+    createTransaction: (apiUrl: string) => `${apiUrl}${base}/Transaction/`,
+    disableTransation: (apiUrl: string, id: string) =>
+      `${apiUrl}${base}/Transaction/${id}`,
 
     createMPSubscription: (apiUrl: string): string =>
       `${apiUrl}${base}/Payment/create-preapproval`,
+
+    calcelMPSubscription: (apiUrl: string, id: string): string =>
+      `${apiUrl}${base}/Payment/cancel-preapproval/${id}`,
+
+    logout: (apiUrl: string): string => `${apiUrl}${base}/User/logout`,
   };
 }
